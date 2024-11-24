ec97a4a36fa8ce2053b59a7d4770535f4700bc10
Abu Masum Didar <masumdidar.cse@gmail.com>
2024-11-24 23:20:21 +0600

    update form and add discount field into product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_24_111040) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_24_113443) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_24_111040) do
     t.decimal "price", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "discount", precision: 10
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
