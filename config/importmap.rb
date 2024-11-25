af166142c0d82ca637510fb357b735285a6ddc3d
Abu Masum Didar <masumdidar.cse@gmail.com>
2024-11-25 19:07:30 +0600

    fix the issue of cdn for importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "font-awesome", to: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+
